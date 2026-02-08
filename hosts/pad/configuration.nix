f33b8427f27fe6904e7c8a962ac9f21c4e8124fd
akmal <isakulovdev@gmail.com>
2026-02-08 16:23:36 +0500

    add pad configs

---
+++ hosts/pad/configuration.nix
@@ -0,0 +1,211 @@
+{
+  pkgs,
+  ...
+}: {
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+    ../../extra/fonts.nix
+  ];
+
+  # Flakes
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Keep the last 10 generation
+  boot.loader.systemd-boot.configurationLimit = 5;
+
+  # Weekly garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  # Auto optimize store
+  nix.settings.auto-optimise-store = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Tashkent";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us,ru";
+    variant = "";
+    options = "grp:lswitch,grp:lshift_toggle";
+  };
+
+  services.flatpak.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.thermald.enable = true;
+
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
+
+  powerManagement.cpuFreqGovernor = "powersave";
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.akmal = {
+    isNormalUser = true;
+    description = "akmal";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+    packages = [
+      #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox = {
+    enable = true;
+    policies = {
+      ExtensionSettings = {
+        default_area = "navbar";
+      };
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowBroken = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    micro
+    curl
+    bat
+    zellij
+    helix
+
+    jujutsu
+
+    rustc
+    cargo
+    gcc
+    pkg-config
+    alejandra
+    zsh
+    fish
+    starship
+    element-desktop
+
+    # fish plugins
+    fishPlugins.done
+    fishPlugins.fzf-fish
+    fishPlugins.forgit
+    fishPlugins.hydro
+    fzf
+    fishPlugins.grc
+    grc
+
+    powertop
+    tlp
+    resources
+    deno
+    zig
+    gnome-screenshot
+    gnome-tweaks
+    gnome-extension-manager
+  ];
+
+  # services.tlp = {
+  #   enable = true;
+  #
+  #   settings = {
+  #     START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+  #     STOP_CHARGE_THRESH_BAT0 = 79; # 80 and above it stops charging
+  #   };
+  # };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gnome];
+    config.common.default = "gnome";
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  programs.zsh.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
