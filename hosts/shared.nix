feb4caceec42433a80c3f02f1af9ce3146975a2b
akmal <isakulovdev@gmail.com>
2026-02-20 12:02:11 +0500

    feat: add spicetify

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -1,8 +1,38 @@
 {
+  inputs,
   pkgs,
   ...
 }:
 {
+  imports = [
+    inputs.spicetify-nix.nixosModules.default
+  ];
+
+  programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+    in
+    {
+      enable = true;
+
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      enabledCustomApps = with spicePkgs.apps; [
+        newReleases
+        ncsVisualizer
+      ];
+      enabledSnippets = with spicePkgs.snippets; [
+        rotatingCoverart
+        pointer
+      ];
+
+      theme = spicePkgs.themes.catppuccin;
+      colorScheme = "mocha";
+    };
+
   # Bootloader.
   boot.loader = {
     grub = {
