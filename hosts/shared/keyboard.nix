1bb948425a4f91b3a5efb2461b4cee27a533d44d
akmal <isakulovdev@gmail.com>
2026-02-26 21:54:38 +0500

    feat: home row mods

---
+++ hosts/shared/keyboard.nix
@@ -0,0 +1,14 @@
+{...}: 
+{
+  services.kanata = {
+    enable = true;
+
+    keyboards = {
+      internalKeyboard = {
+        devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
+        extraDefCfg = "process-unmapped-keys yes";
+        configFile = ./kanata.kbd;
+      };
+    };
+  };
+}
